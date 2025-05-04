7ca0acbc9dc2023e50237ddaf662d15eec487f7f
Rythielly Garcia <rthll@outlook.com>
2025-05-04 02:03:35 -0300

    configurações iniciais do backend

---
+++ Backend/src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// const authRoutes = require('./authRoutes');
+// const alunoRoutes = require('./alunoRoutes');
+// const presencaRoutes = require('./presencaRoutes');
+
+// router.use('/auth', authRoutes);
+// router.use('/alunos', alunoRoutes);
+// router.use('/presencas', presencaRoutes);
+
+module.exports = router;
